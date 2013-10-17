49538aa8b02099196ae22bf322fcfe798438ceb3
Xroft666 <vapyhtin@gmail.com>
2013-10-17 15:05:51 +0200

    fixing bug

--- Assets/Scripts/PlatformMomentum.cs
+++ Assets/Scripts/PlatformMomentum.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlatformMomentum : MonoBehaviour {
@@ -23,15 +24,17 @@ public class PlatformMomentum : MonoBehaviour {
 		_rotation = _transform.rotation;
 	}
 	
-	void Update(){
+	void FixedUpdate(){
 		//here we update the player position to understand his movement to apply momentum
 		if(attachedTo == null){
 			//if it is not attached, we do not need to keep track of position
 			if(recordedPlayerPos != null) recordedPlayerPos.Clear();
 			return;
 		}
-		recordedPlayerPos.Add(_transform.position);
+		recordedPlayerPos.Add(rigidbody.position/*_transform.position*/);
 		while(recordedPlayerPos.Count > frameTrackingAmount) recordedPlayerPos.RemoveAt(0);
+		
+		rigidbody.AddForce(GetMomentumVelocityVector() * velocityMultiplier);
 		
 		_transform.parent = null;
 		_transform.rotation = _rotation;
@@ -54,9 +57,9 @@ public class PlatformMomentum : MonoBehaviour {
 			transform.parent = null;
 			print("Dettached from " + attachedTo.name);
 			//apply momentum force here 
-			//rigidbody.AddForce(GetMomentumVelocityVector() * velocityMultiplier);
+//			rigidbody.AddForce(GetMomentumVelocityVector() * velocityMultiplier);
 			
-			rigidbody.AddForce(col.contacts[0].normal * GetMomentumVelocity() * velocityMultiplier);
+//			rigidbody.AddForce(col.contacts[0].normal * GetMomentumVelocity() * velocityMultiplier);
 			
 			attachedTo = null;
 			
@@ -64,29 +67,44 @@ public class PlatformMomentum : MonoBehaviour {
 		
 	}
 	
-	/*Vector3 GetMomentumVelocityVector(){
-		//right now this is a pretty basic
-		//we should consider all the points from our records to get a more accurate momentum
-		Vector3 initialPositionConsidered = recordedPlayerPos[0];
-		Vector3 lastPositionConsidered = recordedPlayerPos[recordedPlayerPos.Count-1];
+	Vector3 GetMomentumVelocityVector(){
 		
-		Vector3 direction = lastPositionConsidered - initialPositionConsidered;
+		Vector3 result = Vector3.zero;
 		
-		//taking away all movement that pulls the player below because o momentum
-		//might not be good if the player is somehow attached to the bottom of a platform
-		if(direction.y < 0) direction = new Vector3(direction.x, 0f, 0f);
-		//otherwise, cancel z movement anyway
-		else direction = new Vector3(direction.x, direction.y, 0f);
+		if( recordedPlayerPos.Count > 0 )
+		{
+			//right now this is a pretty basic
+			//we should consider all the points from our records to get a more accurate momentum
+			Vector3 initialPositionConsidered = recordedPlayerPos[0];
+			Vector3 lastPositionConsidered = recordedPlayerPos[recordedPlayerPos.Count-1];
+			
+			Vector3 direction = lastPositionConsidered - initialPositionConsidered;
+			
+			//taking away all movement that pulls the player below because o momentum
+			//might not be good if the player is somehow attached to the bottom of a platform
+			if(direction.y < 0) direction = new Vector3(direction.x, 0f, 0f);
+			//otherwise, cancel z movement anyway
+			else direction = new Vector3(direction.x, direction.y, 0f);
+			
+			result = direction;
+			
+			print("Vector force is " + direction.ToString());
+		}
 		
-		print("Vector force is " + direction.ToString());
-		return direction;
-	}*/
+		return result;
+	}
 	
 	float GetMomentumVelocity(){
-		Vector3 initialPositionConsidered = recordedPlayerPos[0];
-		Vector3 lastPositionConsidered = recordedPlayerPos[recordedPlayerPos.Count-1];
 		
-		return (initialPositionConsidered - lastPositionConsidered).magnitude;
+		float result = 0f;
 		
+		if( recordedPlayerPos.Count > 0 ){
+			
+			Vector3 initialPositionConsidered = recordedPlayerPos[0];
+			Vector3 lastPositionConsidered = recordedPlayerPos[recordedPlayerPos.Count-1];
+			result = (initialPositionConsidered - lastPositionConsidered).magnitude;
+		}
+		
+		return result;
 	}
 }
