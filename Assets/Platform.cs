27d0ec7222df54b54822190a925a1bbc08181ac3
Xroft666 <vapyhtin@gmail.com>
2013-11-22 20:13:30 +0100

    Taking out from Bones area to Platform

--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -2,6 +2,26 @@
 using System.Collections.Generic;
 using System;
 
+using System.Linq;
+
+public enum ColorState
+{
+	CS_NOTSELECTED,
+	CS_NEXTTOSELECT,
+	CS_NOTACTIVE,
+	CS_ACTIVE
+}
+
+#region Other Support Classes
+[System.Serializable]
+public class PlatformInfo
+{
+	public float initialAngle;
+	public Vector3 initialScale;
+	
+	public Platform reference;
+}
+
 public class Platform : MonoBehaviour {
 	
 	public PlatformType pt;
@@ -11,10 +31,43 @@ public class Platform : MonoBehaviour {
 	public Rail rail;
 	
 	public Vector3 offset;
-	
+
+	public ColorState colorState;
+
+	private MeshRenderer[] LEDRenderers;
+
 	public void Start(){
+
 		gameObject.layer = transform.position.x < 0 ? LayerMask.NameToLayer("PlatformP1")
 													: LayerMask.NameToLayer("PlatformP2");
+
+		LEDRenderers = 
+			(from renderer in (transform.GetComponentsInChildren<MeshRenderer>() as MeshRenderer[]) 
+			 where ((from material in renderer.materials 
+			        where material.name.Contains( "LED" )
+			        select material).Any<Material>() )		 
+		select renderer).ToArray();
+
+//		foreach( MeshRenderer rend in (transform.GetComponentsInChildren<MeshRenderer>() as MeshRenderer[]) )
+//		{
+//			for( int i = 0; i < rend.sharedMaterials.Length; i++ )
+//			{
+//				if( rend.sharedMaterials[i].name.Contains( "LED" ) )
+//				{
+//					if( LEDMaterial == null )
+//					{
+//						Material newMaterial = new Material(rend.sharedMaterials[i]);
+//						newMaterial.name = "LED Custom Instance";
+//						LEDMaterial = newMaterial;
+//					}
+//
+//
+//					rend.sharedMaterial = LEDMaterial;
+//					print (LEDMaterial.name);
+//					print (rend.sharedMaterial.name);
+//				}
+//			}
+//		}
 
 
 		rail = transform.GetComponentInChildren<Rail>();
@@ -41,6 +94,30 @@ public class Platform : MonoBehaviour {
 			rail = transform.GetChild(0).GetComponent<Rail>();
 			rail.transform.parent = transform.parent;
 		}
+	
+	}
+
+	public void SetColorState( ColorState cs )
+	{
+		colorState = cs;
+
+		foreach( MeshRenderer rend in LEDRenderers )
+		{
+
+			switch( cs )
+			{
+			case ColorState.CS_ACTIVE:
+				rend.materials[1].color = Color.red;
+				break;
+			case ColorState.CS_NEXTTOSELECT:
+				rend.materials[1].color = Color.green;
+				break;
+			case ColorState.CS_NOTACTIVE:
+				rend.materials[1].color = Color.white;
+				break;
+			}
+
+		}
 	}
 	
 	public void OnDrawGizmos(){
@@ -64,3 +141,4 @@ public class Platform : MonoBehaviour {
 		PT_EZLINE
 	}
 }
+#endregion
